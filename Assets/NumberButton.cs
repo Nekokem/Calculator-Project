8448a064292b3d8e50911d8381b26d11823a7ce7
Katie Morley <kamytherabbit@gmail.com>
2017-05-01 08:50:02 -0600

    Added number button script

---
+++ Assets/NumberButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class NumberButton : MonoBehaviour {
+
+	public InputField display;
+	public InputField display2;
+
+	public void AddNumberToDisplay (float number) 
+	{
+
+		if (display == OnPointerClickInput.Inputs [0]) {
+			display.text += number.ToString ();
+
+		} 
+
+		else if (display2 == OnPointerClickInput.Inputs [0])
+		{
+			display2.text += number.ToString ();
+
+		}
+	}
+
+}
