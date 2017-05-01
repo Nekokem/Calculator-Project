dbbf0022305bcc4d783351886731e64ee9df7bdc
Katie Morley <kamytherabbit@gmail.com>
2017-05-01 08:50:23 -0600

    Added OnPointerClick script

---
+++ Assets/OnPointerClickInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OnPointerClickInput : MonoBehaviour {
+
+	public InputField display;
+	public InputField startdisplay;
+
+
+	public static List <InputField> Inputs = new List<InputField>();
+
+	void Start () 
+	{
+	
+		if (Inputs.Count == 0) 
+		{
+			Inputs.Add (startdisplay);
+
+		}
+
+	}
+
+	public void OnPointerClick() 
+	{
+		
+		
+		if (Inputs.Count > 0) 
+		{
+			Inputs.Remove (Inputs [0]);
+			Inputs.Add (display);
+		} 
+		else 
+		{
+			Inputs.Add (display);
+		}
+	}
+}
+
+
