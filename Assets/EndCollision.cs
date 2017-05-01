ba06ab52842e228fc34c9702689841d4c2f53b9c
Katie Morley <kamytherabbit@gmail.com>
2017-05-01 08:50:46 -0600

    Added EndCollision script

---
+++ Assets/EndCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EndCollision : MonoBehaviour {
+
+	public Text gameOverText;
+
+
+	void Start () 
+	{
+		gameOverText.text = "";
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.name == "Plane") 
+		{
+			gameOverText.text = "Game Over!";
+		}
+	}
+		
+}
